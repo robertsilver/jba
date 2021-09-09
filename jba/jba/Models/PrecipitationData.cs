4556d0bf5e713555286f87fbd4499509aca1eb1e
Robert <robert.silver@outlook.com>
2021-09-09 16:50:43 +0100

    Renamed the file to something more meaningful.

---
+++ jba/jba/Models/PrecipitationData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace jba.Models
+{
+    public class PrecipitationData
+    {
+        public int Id { get; set; }
+
+        public double Xref { get; set; }
+
+        public double Yref { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public double Value { get; set; }
+    }
+}
