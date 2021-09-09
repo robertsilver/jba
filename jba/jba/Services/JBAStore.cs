e7b692bdf4c187dfde5e800de8236ca3ce0f0f66
Robert <robert.silver@outlook.com>
2021-09-09 16:52:44 +0100

    Code written to save the data to the DB using a SP and user defined table type.

---
+++ jba/jba/Services/JBAStore.cs
@@ -0,0 +1,31 @@
+﻿using jba.Models;
+using JBAUtils;
+using System.Collections.Generic;
+using System.Data;
+
+namespace jba.Services
+{
+    public class JBAStore : IJBAStore
+    {
+        DataAccessService _dbContext = new DataAccessService();
+
+        public JBAStore()
+        {
+            _dbContext.ExecStoredProcedure<PrecipitationData>("CreateTablesIfNecessary", new { });
+        }
+
+        public void StoreData(List<PrecipitationData> data)
+        {
+            DataTable rainDt = _dbContext.CreateAndPopulateTableForList(data, new
+            {
+                Xref = "Xref",
+                Yref = "Yref",
+                Date = "Date",
+                Value = "Value"
+            });
+
+            _dbContext.ExecStoredProcedure<PrecipitationData>("SavePrecipitationData", new { rainData = rainDt });
+
+        }
+    }
+}
