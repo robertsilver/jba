525432b78c6a0e32021917e6129229758ad02100
Robert <robert.silver@outlook.com>
2021-09-09 10:41:37 +0100

    Created the enum for the solution

---
+++ jba/jba/Models/FileValues.cs
@@ -0,0 +1,12 @@
+﻿namespace jba.Models
+{
+    public enum FileValues
+    {
+        Longitude = 0,
+        Latitude = 1,
+        GridXY = 2,
+        Years = 3,
+        GridRef = 4,
+        Data = 5
+    }
+}
