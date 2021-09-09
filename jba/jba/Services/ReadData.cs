114ed72f38b19b84bad65eac88978ee688d274ea
Robert <robert.silver@outlook.com>
2021-09-09 11:33:48 +0100

    Created code to read the data lines.

---
+++ jba/jba/Services/ReadData.cs
@@ -0,0 +1,36 @@
+﻿using jba.Models;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace jba.Services
+{
+    public class ReadData : ProcessFile
+    {
+        public override List<int> ReadDataLine(string line)
+        {
+            Regex regex = new Regex(@"^\d$");
+            List<int> precipitation = null;
+
+            if (!regex.IsMatch(line.TrimStart().Substring(0, 1)))
+                return precipitation;
+
+            precipitation = new List<int>();
+
+            foreach (var number in line.Split(' '))
+            {
+                if (string.IsNullOrEmpty(number))
+                    continue;
+
+                precipitation.Add(Convert.ToInt32(number));
+            }
+
+            return precipitation;
+        }
+
+        public override double[] ReadFile(string line)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
